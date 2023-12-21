5e318d48fbc1c2ef5f3fcc9735b8bd7466bbfc20
Avi Kumar <149712920+frogsjumps@users.noreply.github.com>
2023-12-21 12:41:39 -0800

    Add files via upload

---
+++ AK.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("How many students are in your class: ");
+
+int students = Convert.ToInt32(Console.ReadLine());
+string[] people = new string[students];
+
+Console.WriteLine("What are their names?: ");
+
+for (int i = 0; i < people.Length; i++)
+{
+    people[i] = Console.ReadLine();
+}
+
+Console.WriteLine("------------------------");
+Array.Sort(people);
+
+for (int i = 0; i < people.Length; i++)
+{
+    Console.WriteLine(people[i]);
+}
+
+Console.ReadKey();
